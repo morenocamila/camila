5b78ae114a9d8c51ece0868604920e8d70ad82a7
morenocamila <and.clmoreno29@gmail.com>
2023-06-30 07:50:18 -0500

    first commit

---
+++ proceso.js
@@ -0,0 +1,5 @@
+function tico() {
+    document.getElementById('lifo').innerHTML= ""
+    +"Los colores, la danza y la natureleza de Brasil han marcadouna austera ceremonia inaugural del Mundial Brasil 2014, que contó con la presentación de la música del certamen, We are One, interpretada por Jennifer López, Pitbull y la brasileña Claudia Leitte."
++"Con coreografía de la belga Daphné Cornez, el campo de juego estuvo representado por bailarines disfrazados de flores, plantas e instrumentos de percusión típicos del samba y otros ritmos del país, asi como todas las regiones, desde las playas al frío sur, de la Amazonia al semiárido nordestino.";
+}
